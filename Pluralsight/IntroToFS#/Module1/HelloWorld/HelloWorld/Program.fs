897dca76037f25a14f8bc0a4b0ef8ffdc791bb27
Nemesek <dnemesek@gmail.com>
2015-04-26 21:21:02 -0500

    first couple of examples

---
+++ Pluralsight/IntroToFS#/Module1/HelloWorld/HelloWorld/Program.fs
@@ -0,0 +1,15 @@
+﻿//// Learn more about F# at http://fsharp.net
+//// See the 'F# Tutorial' project for more help.
+//
+//[<EntryPoint>]
+//let main argv = 
+//    printfn "%A" argv
+//    0 // return an integer exit code
+
+printfn "Hello World!"
+
+printfn "How are you today?"
+
+let response = System.Console.ReadLine()
+
+printfn "Great to hear you're %s" response
---
+++ Pluralsight/IntroToFS#/Module1/HelloWorldWinForms/HelloWorldWinForms/Program.fs
@@ -0,0 +1,18 @@
+﻿open System.Windows.Forms
+
+type Person = {Name: string; Age: int} // Record data type
+
+let testData = 
+    [|
+        {Name= "Harry"; Age=37};
+        {Name= "July"; Age=41}
+    |]
+
+
+// F# convention is to new up classes that implement IDisposable
+let form = new Form(Text = "F# Windows Form")
+
+let dataGrid = new DataGridView(Dock=DockStyle.Fill, DataSource = testData)
+form.Controls.Add(dataGrid)
+
+Application.Run(form)
