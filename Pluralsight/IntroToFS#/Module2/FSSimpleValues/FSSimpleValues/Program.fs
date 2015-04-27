3b8393cbb8aa895f7edc7d7302596dd3116562d8
nemesek <dnemesek@gmail.com>
2015-04-27 16:55:49 -0500

    starting module 2

---
+++ Pluralsight/IntroToFS#/Module2/FSSimpleValues/FSSimpleValues/Program.fs
@@ -0,0 +1,16 @@
+﻿let x = 100 // think of them as values not variables, immutable
+
+let strVal = "hi there"
+
+let boolVal = true
+
+let floatVal = 12.3
+
+let decimalVal = 47.99m
+
+let square(x) = x * x
+let square2 x = x * x
+
+let add x y =
+    x + y
+
